b66074d57b926aaa7306e2a7c14816bdeef19586
utsarga01 <regmiutsarga7@gmail.com>
2024-02-20 21:48:19 +0545

    test

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -9,47 +9,24 @@ function LoginPage() {
   };
 
   return (
-    <div className="bg-gray-100 flex justify-center items-center h-screen">
+
+    <div className="bg-blue-300 justify-center items-center h-screen">
+        <div className='text-black'style={{ textAlign: 'center', marginTop: '20px' }}>
+        <h1 ClassName='w-full text-3xl font-bold text-[#00df9a]'style={{ fontWeight: 'bold' }}>HANDSIGN RECOGNITION SYSTEM.</h1>
+        </div>
       {!showWebcam ? (
-        <div className="bg-white p-8 rounded shadow-md w-96">
-          <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <div className="bg-white p-8 rounded shadow-md w-96 flex items-center justify-center">
+          
+        
           <form>
-            <div className="mb-4">
-              <label
-                htmlFor="username"
-                className="block text-gray-700 font-semibold mb-2"
-              >
-                Username
-              </label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Enter your username"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="password"
-                className="block text-gray-700 font-semibold mb-2"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter your password"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-              />
-            </div>
+          
+         
             <button
               type="button"
               onClick={handleLoginClick}
               className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
             >
-              Login
+            Get started
             </button>
           </form>
         </div>
