8aa24dbaf7057b176a030dcf75aeefac88c074cc
Kushagra <kushagra.acharya@outlook.com>
2024-02-20 22:03:37 +0545

    prabin fixes

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -9,31 +9,59 @@ function LoginPage() {
   };
 
   return (
+    <div class="flex justify-center items-center h-screen">
+      <div class="text-center">
+        <div className="w-full text-2xl font-semibold">
+          HANDSIGN RECOGNITION SYSTEM
+        </div>
 
-    <div className="bg-blue-300 justify-center items-center h-screen">
-        <div className='text-black'style={{ textAlign: 'center', marginTop: '20px' }}>
-        <h1 ClassName='w-full text-3xl font-bold text-[#00df9a]'style={{ fontWeight: 'bold' }}>HANDSIGN RECOGNITION SYSTEM.</h1>
-        </div>
-      {!showWebcam ? (
-        <div className="bg-white p-8 rounded shadow-md w-96 flex items-center justify-center">
-          
-        
-          <form>
-          
-         
-            <button
-              type="button"
-              onClick={handleLoginClick}
-              className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
-            >
-            Get started
-            </button>
-          </form>
-        </div>
-      ) : (
-        <WebcamCapture />
-      )}
+        <div className="h-20">{""}</div>
+
+        {!showWebcam ? (
+          <div className=" flex items-center justify-center">
+            <form>
+              <button
+                type="button"
+                onClick={handleLoginClick}
+                className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+              >
+                Get started
+              </button>
+            </form>
+          </div>
+        ) : (
+          <WebcamCapture />
+        )}
+      </div>
     </div>
+    // <div className="bg-blue-300 justify-center items-center h-screen">
+    //   <div
+    //     className="text-black"
+    //     style={{ textAlign: "center", marginTop: "20px" }}
+    //   >
+    //     <h1
+    //       ClassName="w-full text-3xl font-bold text-[#00df9a]"
+    //       style={{ fontWeight: "bold" }}
+    //     >
+    //       HANDSIGN RECOGNITION SYSTEM.
+    //     </h1>
+    //   </div>
+    //   {!showWebcam ? (
+    //     <div className="bg-white p-8 rounded shadow-md w-96 flex items-center justify-center">
+    //       <form>
+    //         <button
+    //           type="button"
+    //           onClick={handleLoginClick}
+    //           className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+    //         >
+    //           Get started
+    //         </button>
+    //       </form>
+    //     </div>
+    //   ) : (
+    //     <WebcamCapture />
+    //   )}
+    // </div>
   );
 }
 
